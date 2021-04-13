7b0c9209f7be37f86770fe08b2faf88dc316c931
Yuta Hagiwara <yhagiwara@outlook.com>
2021-04-13 17:59:44 -0400

    add insertNode

--- tree/binaryTree.py
+++ tree/binaryTree.py
@@ -10,12 +10,11 @@ class binaryTree:
 
     def insertBst(self, num, head):
         tree.root = self.insertNode(num, head)
-
+        
     def insertNode(self, num, node):
         
         if node is None:
-            new_node = binaryTreeNode()
-            new_node.data = num
+            new_node = binaryTreeNode(num)
             node = new_node
             return node
 
@@ -24,9 +23,25 @@ class binaryTree:
 
         elif num > node.data:
             node.right = self.insertNode(num, node.right)
-
         return node
 
-tree = binaryTree(1)
+    def printTree(self, root): 
+        if root is not None:
+            self.printTree(root.left)
+            print(root.data)
+            self.printTree(root.right)
+            print(root.data)
+        
 
-print(tree.root.data)
+tree = binaryTree(5)
+tree.insertBst(9,tree.root)
+tree.insertBst(7,tree.root)
+tree.insertBst(3,tree.root)
+tree.insertBst(8,tree.root)
+tree.insertBst(12,tree.root)
+
+tree.printTree(tree.root)
+
+# print(tree.root.data)
+# print(tree.root.right.data)
+# print(tree.root.right.right.data)
