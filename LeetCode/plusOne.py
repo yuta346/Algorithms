8a233775569c2493c963ef1349936088e260920a
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-04-18 15:39:03 -0400

    delete comment

--- LeetCode/plusOne.py
+++ LeetCode/plusOne.py
@@ -23,19 +23,6 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """ 
-        # flag = False
-
-        # for j in range(len(digits)):
-        #     print(digits[j])
-        #     if digits[j]==0:
-        #         flag = True
-        #     else:
-        #         break
-
-        # if flag==True: 
-        #     digits.insert(0,1)
-        #     return digits
-
         for i in range(len(digits)-1,-1,-1):
             if digits[i] != 9:
                 digits[i]+=1
