1812fd48454b05759c453b59ca7d1ccca536e85b
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-05-04 22:29:51 -0400

    iterative solution

--- LeetCode/n-aryTreePostorderTraversal.py
+++ LeetCode/n-aryTreePostorderTraversal.py
@@ -1,5 +1,6 @@
 """
 # Definition for a Node.
+# Definition for a Node.
 class Node(object):
     def __init__(self, val=None, children=None):
         self.val = val
@@ -12,3 +13,13 @@ class Solution(object):
         :type root: Node
         :rtype: List[int]
         """
+        if root is None:
+            return root
+        result = []
+        stack = [root]
+        while stack:
+            current = stack.pop()
+            result.insert(0,current.val)
+            for neighbor in current.children:
+                stack.append(neighbor)
+        return result
