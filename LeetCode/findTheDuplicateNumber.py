1b72a9c18569caa951e4fe59095d954c1603a5e0
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-05-23 18:19:52 -0400

    initial commit

---
+++ LeetCode/findTheDuplicateNumber.py
@@ -0,0 +1,25 @@
+
+# Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+# There is only one repeated number in nums, return this repeated number.
+
+# You must solve the problem without modifying the array nums and uses only constant extra space.
+
+
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_dict = {}
+        for i in nums:
+            nums_dict[i] = nums_dict.get(i,0)+1
+            if nums_dict[i] > 1:
+                return i
+
+
+
+sol = Solution()
+print(sol.findDuplicate([1,3,4,2,2]))
