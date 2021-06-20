e43aba785bf670c44dc80f78a093f953b91a595d
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-06-20 00:24:43 -0700

    initial commit

---
+++ LeetCode/pascalsTriangle.py
@@ -0,0 +1,18 @@
+# Input: numRows = 5
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+# Input: numRows = 1
+# Output: [[1]]
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
+        
+sol = Solution()
+sol.generate(5)
+# sol.generate(1)
+
+
